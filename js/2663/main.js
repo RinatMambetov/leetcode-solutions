d39a8bfa5e1a1710d9e1e5e720f3fee838af998b
rinat <mambetovpost@gmail.com>
2023-05-06 16:25:16 +0300

    +2663

---
+++ js/2663/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function () {
+  return function (...args) {
+    return "Hello World";
+  }
+};
+
+
+const f = createHelloWorld();
+console.log(f([])); // "Hello World"
+console.log(f({}, null, 42)); // "Hello World"
